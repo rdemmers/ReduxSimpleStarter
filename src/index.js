f759c04efb056642060704ae41e9ed6e809cce9a
rdemmers <chibitenshin@gmail.com>
2018-01-26 15:48:39 +0100

    Finished up section 2 of the course

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
 
-const API_KEY = "AIzaSyArJr58hStFHwEwMRvmeT6wonO5JbPR4p8";
+const API_KEY = "AIzaSyD_iM6haxzLddRPYVXU6FNn5P51u0-6VTI";
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('overwatch');
+  }
+
+
+  videoSearch(term){
+    YTSearch({
+      key: API_KEY,
+      term: term
+    }, (videos) => {
+      this.setState({videos: videos, selectedVideo: videos[0]})
+
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400);
+
+    return (<div>
+      <SearchBar onSearchTermChange={videoSearch}/>
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+    </div>);
+  }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
